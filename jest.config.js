d74b5e184ba62a5a0cf120696d453e559c63cfe3
maziel <maziel.silva@gmail.com>
2025-02-17 17:50:29 -0300

    novo test

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,13 @@
 import 'jest-preset-angular/setup-jest';
+
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest'
+    }
+};
